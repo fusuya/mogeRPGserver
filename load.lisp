45e0675d282e117177ae6ae861fbcf0da91f2727
fusuya <kaibutsu_oujo@excite.co.jp>
2017-10-17 15:37:03 +0900

    loadしてmain関数実行

---
+++ load.lisp
@@ -0,0 +1,24 @@
+(ql:quickload :jonathan)
+(ql:quickload :cl-ppcre)
+(load "orc-battle.lisp" :external-format :utf-8)
+(load "maze-test.lisp" :external-format :utf-8)
+#|
+;;デバッガ起動したくないときに使う
+;; デバッガフックを設定
+(setf sb-ext:*invoke-debugger-hook*  
+      (lambda (condition hook) 
+        (declare (ignore condition hook))
+        ;; デバッガが呼ばれたら、単にプログラムを終了する
+        ;; recklessly-p に t を指定して、後始末(標準出力ストリームのフラッシュ等)が行われないようにする
+        (sb-ext:quit :recklessly-p t)))
+|#
+
+(main)
+(sb-ext:exit)
+#|
+(sb-ext:save-lisp-and-die "mogerpg"
+			  :toplevel #'main
+			  :save-runtime-options t
+			  :executable t)
+|#
+
